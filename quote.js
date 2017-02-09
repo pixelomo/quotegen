d37328b0e3564ad65cf5cc1324cf55f7703ed5d8
Alan Sutherland <alan.sutherland@liquidshop.com>
2017-02-09 17:41:19 +0000

    Update quote.js

--- quote.js
+++ quote.js
@@ -1,153 +1,99 @@
 var quoteMaker = {
         data : {
-            'services':null,
-            'bedrooms':null,
-            'furnishing':null,
-            'quoteString':''
+            'services':    null,
+            'bedrooms':    null,
+            'furnishing':  null,
+            'quoteString': ''
         },
-        servicesListener : function(){
-           this.data.services = document.getElementById('service').options[document.getElementById('service').selectedIndex].text;
-        },
-        bedroomsListener : function(){
-           this.data.bedrooms = document.getElementById('bedrooms').options[document.getElementById('bedrooms').selectedIndex].text;
-        },
-        furnishingListener : function(){
-           this.data.furnishing = document.getElementById('furnishing').options[document.getElementById('furnishing').selectedIndex].text;
-        },
-        changeData : function(divObj){
-            this.data.quoteString='';
-            if( divObj.id == 'service' ){
-                this.servicesListener();
-            }else if( divObj.id == 'bedrooms' ){
-                this.bedroomsListener();
-            }else if( divObj.id == 'furnishing' ){
-                this.furnishingListener();
-            }
-            this.updateQuote();
-        },
-    updateQuote : function() {
-      if (this.data.services == "Inventory" &&this.data.bedrooms =="1" && this.data.furnishing == "Furnished") 
-         this.data.quoteString = "= £70"; 
-      if (this.data.services == "Inventory" &&this.data.bedrooms =="2" && this.data.furnishing == "Furnished") 
-         this.data.quoteString = "= £75";
-      if (this.data.services == "Inventory" &&this.data.bedrooms =="3" && this.data.furnishing == "Furnished") 
-         this.data.quoteString = "= £80";
-      if (this.data.services == "Inventory" &&this.data.bedrooms =="4" && this.data.furnishing == "Furnished") 
-         this.data.quoteString = "= £85";
-      if (this.data.services == "Inventory" &&this.data.bedrooms =="5" && this.data.furnishing == "Furnished") 
-         this.data.quoteString = "= £90";
-      if (this.data.services == "Inventory" &&this.data.bedrooms =="6+" && this.data.furnishing == "Furnished") 
-         this.data.quoteString = "= £100";
+    
+  function servicesListener(){
+     this.data.services = document.getElementById('service').options[document.getElementById('service').selectedIndex].text;
+  },
 
-      if (this.data.services == "Inventory" &&this.data.bedrooms =="1" && this.data.furnishing == "Part Furnished") 
-         this.data.quoteString = "= £60"; 
-      if (this.data.services == "Inventory" &&this.data.bedrooms =="2" && this.data.furnishing == "Part Furnished") 
-         this.data.quoteString = "= £65";
-      if (this.data.services == "Inventory" &&this.data.bedrooms =="3" && this.data.furnishing == "Part Furnished") 
-         this.data.quoteString = "= £70";
-      if (this.data.services == "Inventory" &&this.data.bedrooms =="4" && this.data.furnishing == "Part Furnished") 
-         this.data.quoteString = "= £75";
-      if (this.data.services == "Inventory" &&this.data.bedrooms =="5" && this.data.furnishing == "Part Furnished") 
-         this.data.quoteString = "= £80";
-      if (this.data.services == "Inventory" &&this.data.bedrooms =="6+" && this.data.furnishing == "Part Furnished") 
-         this.data.quoteString = "= £85";
+  function bedroomsListener(){
+     this.data.bedrooms = document.getElementById('bedrooms').options[document.getElementById('bedrooms').selectedIndex].text;
+  },
 
-      if (this.data.services == "Inventory" &&this.data.bedrooms =="1" && this.data.furnishing == "Unfurnished (except for white goods)") 
-         this.data.quoteString = "= £55"; 
-      if (this.data.services == "Inventory" &&this.data.bedrooms =="2" && this.data.furnishing == "Unfurnished (except for white goods)") 
-         this.data.quoteString = "= £60";
-      if (this.data.services == "Inventory" &&this.data.bedrooms =="3" && this.data.furnishing == "Unfurnished (except for white goods)") 
-         this.data.quoteString = "= £65";
-      if (this.data.services == "Inventory" &&this.data.bedrooms =="4" && this.data.furnishing == "Unfurnished (except for white goods)") 
-         this.data.quoteString = "= £70";
-      if (this.data.services == "Inventory" &&this.data.bedrooms =="5" && this.data.furnishing == "Unfurnished (except for white goods)") 
-         this.data.quoteString = "= £75";
-      if (this.data.services == "Inventory" &&this.data.bedrooms =="6+" && this.data.furnishing == "Unfurnished (except for white goods)") 
-         this.data.quoteString = "= £80";
+  function furnishingListener(){
+     this.data.furnishing = document.getElementById('furnishing').options[document.getElementById('furnishing').selectedIndex].text;
+  },
 
+  function changeData(divObj){
 
-      if (this.data.services == "Check-In" &&this.data.bedrooms =="1" && this.data.furnishing == "Furnished") 
-         this.data.quoteString = "= £50"; 
-      if (this.data.services == "Check-In" &&this.data.bedrooms =="2" && this.data.furnishing == "Furnished") 
-         this.data.quoteString = "= £55";
-      if (this.data.services == "Check-In" &&this.data.bedrooms =="3" && this.data.furnishing == "Furnished") 
-         this.data.quoteString = "= £60";
-      if (this.data.services == "Check-In" &&this.data.bedrooms =="4" && this.data.furnishing == "Furnished") 
-         this.data.quoteString = "= £65";
-      if (this.data.services == "Check-In" &&this.data.bedrooms =="5" && this.data.furnishing == "Furnished") 
-         this.data.quoteString = "= £70";
-      if (this.data.services == "Check-In" &&this.data.bedrooms =="6+" && this.data.furnishing == "Furnished") 
-         this.data.quoteString = "= £75";
+      this.data.quoteString = '';
 
-      if (this.data.services == "Check-In" &&this.data.bedrooms =="1" && this.data.furnishing == "Part Furnished") 
-         this.data.quoteString = "= £40"; 
-      if (this.data.services == "Check-In" &&this.data.bedrooms =="2" && this.data.furnishing == "Part Furnished") 
-         this.data.quoteString = "= £45";
-      if (this.data.services == "Check-In" &&this.data.bedrooms =="3" && this.data.furnishing == "Part Furnished") 
-         this.data.quoteString = "= £50";
-      if (this.data.services == "Check-In" &&this.data.bedrooms =="4" && this.data.furnishing == "Part Furnished") 
-         this.data.quoteString = "= £55";
-      if (this.data.services == "Check-In" &&this.data.bedrooms =="5" && this.data.furnishing == "Part Furnished") 
-         this.data.quoteString = "= £60";
-      if (this.data.services == "Check-In" &&this.data.bedrooms =="6+" && this.data.furnishing == "Part Furnished") 
-         this.data.quoteString = "= £65";
+      if( divObj.id == 'service' ){
+          this.servicesListener();
+      } else if ( divObj.id == 'bedrooms' ){
+          this.bedroomsListener();
+      } else if ( divObj.id == 'furnishing' ){
+          this.furnishingListener();
+      }
+      this.updateQuote();
+  },
 
-      if (this.data.services == "Check-In" &&this.data.bedrooms =="1" && this.data.furnishing == "Unfurnished (except for white goods)") 
-         this.data.quoteString = "= £35"; 
-      if (this.data.services == "Check-In" &&this.data.bedrooms =="2" && this.data.furnishing == "Unfurnished (except for white goods)") 
-         this.data.quoteString = "= £40";
-      if (this.data.services == "Check-In" &&this.data.bedrooms =="3" && this.data.furnishing == "Unfurnished (except for white goods)") 
-         this.data.quoteString = "= £45";
-      if (this.data.services == "Check-In" &&this.data.bedrooms =="4" && this.data.furnishing == "Unfurnished (except for white goods)") 
-         this.data.quoteString = "= £50";
-      if (this.data.services == "Check-In" &&this.data.bedrooms =="5" && this.data.furnishing == "Unfurnished (except for white goods)") 
-         this.data.quoteString = "= £55";
-      if (this.data.services == "Check-In" &&this.data.bedrooms =="6+" && this.data.furnishing == "Unfurnished (except for white goods)") 
-         this.data.quoteString = "= £60";
+  function updateQuote(service, rooms, furnished){
 
+    const price = 35,
+          service = this.data.services,
+          rooms = this.data.bedrooms,
+          furnished = this.data.furnishing;
 
-      if (this.data.services == "Check-Out" &&this.data.bedrooms =="1" && this.data.furnishing == "Furnished") 
-         this.data.quoteString = "= £55"; 
-      if (this.data.services == "Check-Out" &&this.data.bedrooms =="2" && this.data.furnishing == "Furnished") 
-         this.data.quoteString = "= £60";
-      if (this.data.services == "Check-Out" &&this.data.bedrooms =="3" && this.data.furnishing == "Furnished") 
-         this.data.quoteString = "= £65";
-      if (this.data.services == "Check-Out" &&this.data.bedrooms =="4" && this.data.furnishing == "Furnished") 
-         this.data.quoteString = "= £70";
-      if (this.data.services == "Check-Out" &&this.data.bedrooms =="5" && this.data.furnishing == "Furnished") 
-         this.data.quoteString = "= £75";
-      if (this.data.services == "Check-Out" &&this.data.bedrooms =="6+" && this.data.furnishing == "Furnished") 
-         this.data.quoteString = "= £80";
-
-      if (this.data.services == "Check-Out" &&this.data.bedrooms =="1" && this.data.furnishing == "Part Furnished") 
-         this.data.quoteString = "= £45"; 
-      if (this.data.services == "Check-Out" &&this.data.bedrooms =="2" && this.data.furnishing == "Part Furnished") 
-         this.data.quoteString = "= £50";
-      if (this.data.services == "Check-Out" &&this.data.bedrooms =="3" && this.data.furnishing == "Part Furnished") 
-         this.data.quoteString = "= £55";
-      if (this.data.services == "Check-Out" &&this.data.bedrooms =="4" && this.data.furnishing == "Part Furnished") 
-         this.data.quoteString = "= £60";
-      if (this.data.services == "Check-Out" &&this.data.bedrooms =="5" && this.data.furnishing == "Part Furnished") 
-         this.data.quoteString = "= £65";
-      if (this.data.services == "Check-Out" &&this.data.bedrooms =="6+" && this.data.furnishing == "Part Furnished") 
-         this.data.quoteString = "= £70";
-
-      if (this.data.services == "Check-Out" &&this.data.bedrooms =="1" && this.data.furnishing == "Unfurnished (except for white goods)") 
-         this.data.quoteString = "= £40"; 
-      if (this.data.services == "Check-Out" &&this.data.bedrooms =="2" && this.data.furnishing == "Unfurnished (except for white goods)") 
-         this.data.quoteString = "= £45";
-      if (this.data.services == "Check-Out" &&this.data.bedrooms =="3" && this.data.furnishing == "Unfurnished (except for white goods)") 
-         this.data.quoteString = "= £50";
-      if (this.data.services == "Check-Out" &&this.data.bedrooms =="4" && this.data.furnishing == "Unfurnished (except for white goods)") 
-         this.data.quoteString = "= £55";
-      if (this.data.services == "Check-Out" &&this.data.bedrooms =="5" && this.data.furnishing == "Unfurnished (except for white goods)") 
-         this.data.quoteString = "= £60";
-      if (this.data.services == "Check-Out" &&this.data.bedrooms =="6+" && this.data.furnishing == "Unfurnished (except for white goods)") 
-         this.data.quoteString = "= £65";
-    document.getElementById("quote").innerHTML=this.data.quoteString;
-},
-        init: function(){
-
-        }   
+    // Check-In 50 furnished 40 part furnished 35 unfurnished
+    // Check-Out 55 furnished 45 part furnished 40 unfurnished
+    // Inventory 70 furnished 60 part furnished 55 unfurnished
+    switch(service) {
+      case 'Check-In':
+          price = 35;
+          break;
+      case 'Check-Out':
+          price = 40
+          break;
+      case 'Inventory':
+          price = 55
+          break;
     }
-    quoteMaker.init();
+    // add 5 per room, from 1-6 rooms
+    switch(rooms) {
+      case 1:
+          price = price;
+          break;
+      case 2:
+          price = price + 5;
+          break;
+      case 3:
+          price = price + 10;
+          break;
+      case 4:
+          price = price + 15;
+          break;
+      case 5:
+          price = price + 20;
+          break;
+      case 6:
+          price = price + 25;
+          break;
+    }
+
+    switch(furnished) {
+      case 'Unfurnished':
+          price = price + 5;
+          break;
+      case 'Part Furnished':
+          price = price + 10;
+          break;
+      case 'Furnished':
+          price = price + 15;
+          break;
+    }
+
+    this.data.quoteString = price;
+
+    document.getElementById("quote").innerHTML = this.data.quoteString;
+  },
+
+  function init(){}
+
+}
+
+quoteMaker.init();
